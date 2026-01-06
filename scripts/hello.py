3f3dcab898237a7b596e542c9aebadba6b2ff2a9
NunezX <nunez8517@gmail.com>
2026-01-06 11:06:18 -0300

    edicao script

--- scripts/hello.py
+++ scripts/hello.py
@@ -2,7 +2,7 @@
 user = input('Qual é o seu nome ? ')
 def teste():
     print("Olá, {}!".format(user))
-    print("Servidor pessoal em construção 🚀")
+    print("Servidor pessoal em construção")
 
 if 1 == 1:
     teste()
